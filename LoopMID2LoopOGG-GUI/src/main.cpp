d9bfb6936bff96266f3acd3abbe1e3623b983db7
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2016-12-14 16:53:49 +0900

    add fusion style and resource definition

--- LoopMID2LoopOGG-GUI/src/main.cpp
+++ LoopMID2LoopOGG-GUI/src/main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QSettings>
+#include <QStyleFactory>
 #include <QDebug>
 
 int main(int argc, char *argv[])
@@ -9,6 +10,30 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("WhiteLuckers");
     QCoreApplication::setApplicationName("LoopMID2LoopOGG");
     MainWindow w;
+
+    // set fusion style
+    //     from awesome gist: https://gist.github.com/Skyrpex/5547015
+    a.setStyle(QStyleFactory::create("fusion"));
+
+    QPalette palette;
+    palette.setColor(QPalette::Window, QColor(53,53,53));
+    palette.setColor(QPalette::WindowText, Qt::white);
+    palette.setColor(QPalette::Base, QColor(15,15,15));
+    palette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+    palette.setColor(QPalette::ToolTipBase, Qt::white);
+    palette.setColor(QPalette::ToolTipText, Qt::white);
+    palette.setColor(QPalette::Text, Qt::white);
+    palette.setColor(QPalette::Button, QColor(53,53,53));
+    palette.setColor(QPalette::ButtonText, Qt::white);
+    palette.setColor(QPalette::BrightText, Qt::red);
+
+    palette.setColor(QPalette::Highlight, QColor(142,45,197).lighter());
+    palette.setColor(QPalette::HighlightedText, Qt::black);
+
+    palette.setColor(QPalette::Disabled, QPalette::Text, Qt::darkGray);
+    palette.setColor(QPalette::Disabled, QPalette::ButtonText, Qt::darkGray);
+
+    a.setPalette(palette);
 
     w.show();
 
