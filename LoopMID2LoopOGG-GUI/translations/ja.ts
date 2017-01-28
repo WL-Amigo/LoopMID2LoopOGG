5af7660b5ab2e9ca800632b2f1379de521b8456e
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2017-01-28 20:48:44 +0900

    update translations (jp)

--- LoopMID2LoopOGG-GUI/translations/ja.ts
+++ LoopMID2LoopOGG-GUI/translations/ja.ts
@@ -2,6 +2,90 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ja_JP" sourcelanguage="en_US">
 <context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="14"/>
+        <source>Preference</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="73"/>
+        <source>Output</source>
+        <translation>出力形式</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="110"/>
+        <source>About</source>
+        <translation>このソフトについて</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="167"/>
+        <source>Output File Format</source>
+        <translation>出力ファイル形式</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="173"/>
+        <source>Ogg Vorbis (with loop tag)</source>
+        <translation>Ogg Vorbis (ループタグ付き)</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="180"/>
+        <source>Wave</source>
+        <translation>Waveファイル</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="190"/>
+        <source>Output Mode</source>
+        <translation>出力モード</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="196"/>
+        <source>Optimized mode (for game use)</source>
+        <translation>サイズ節約モード (ゲーム素材として利用したい方向け)</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="203"/>
+        <source>Options for optimized mode</source>
+        <translation>サイズ節約モードのオプション</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="209"/>
+        <source>minimum samples after end of loop</source>
+        <translation>ループ終了後に残す最大サンプル数</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="222"/>
+        <source>Sound-track mode (for listening use)</source>
+        <translation>サウンドトラックモード (鑑賞したい方向け)</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="229"/>
+        <source>Options for sound-track mode</source>
+        <translation>サウンドトラックモードのオプション</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="235"/>
+        <source>number of loop</source>
+        <translation>出力に含めるループ回数</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="245"/>
+        <source>fade-out length (sec.)</source>
+        <translation>フェードアウト秒数</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="299"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;LoopMID 2 LoopOGG&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:6pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Presented by &lt;/span&gt;&lt;a href=&quot;https://wl-amigo.github.io/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#007af4;&quot;&gt;WhiteLuckers&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
     <name>ConversionConfirmationDialog</name>
     <message>
         <source>Dialog</source>
@@ -27,7 +111,7 @@
         <translation>変換中…</translation>
     </message>
     <message>
-        <location filename="../src/ConvertingDialog.cpp" line="40"/>
+        <location filename="../src/ConvertingDialog.cpp" line="37"/>
         <source>Finished!</source>
         <translation>変換完了！</translation>
     </message>
@@ -60,14 +144,74 @@
         <translation>ファイル</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="115"/>
+        <location filename="../forms/mainwindow.ui" line="117"/>
         <source>Exit</source>
         <translation>終了</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="32"/>
+        <location filename="../forms/mainwindow.ui" line="122"/>
+        <source>Config</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="35"/>
         <source>Select Output Direcotry...</source>
         <translation>出力先ディレクトリの選択...</translation>
+    </message>
+</context>
+<context>
+    <name>MultiConvertingDialog</name>
+    <message>
+        <location filename="../forms/MultiConvertingDialog.ui" line="14"/>
+        <source>Multi file converting...</source>
+        <translation>複数ファイルを変換中…</translation>
+    </message>
+    <message>
+        <location filename="../forms/MultiConvertingDialog.ui" line="25"/>
+        <source>Converting...</source>
+        <translation>変換中…</translation>
+    </message>
+    <message>
+        <location filename="../forms/MultiConvertingDialog.ui" line="37"/>
+        <source>Progress:</source>
+        <translation>完了した数:</translation>
+    </message>
+    <message>
+        <location filename="../forms/MultiConvertingDialog.ui" line="47"/>
+        <source>%v / %m</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../forms/MultiConvertingDialog.ui" line="69"/>
+        <source>MIDI File</source>
+        <translation>MIDIファイル</translation>
+    </message>
+    <message>
+        <location filename="../forms/MultiConvertingDialog.ui" line="74"/>
+        <source>Result</source>
+        <translation>状態</translation>
+    </message>
+    <message>
+        <location filename="../src/MultiConvertingDialog.cpp" line="70"/>
+        <source>pending or converting...</source>
+        <translation>変換待機中 or 変換中...</translation>
+    </message>
+    <message>
+        <location filename="../src/MultiConvertingDialog.cpp" line="103"/>
+        <source>Finished!</source>
+        <translation>変換完了！</translation>
+    </message>
+    <message>
+        <location filename="../src/MultiConvertingDialog.cpp" line="111"/>
+        <source>Done!</source>
+        <comment>single work</comment>
+        <translation>完了！</translation>
+    </message>
+    <message>
+        <location filename="../src/MultiConvertingDialog.cpp" line="116"/>
+        <source>Failed by some error...</source>
+        <comment>single work</comment>
+        <translation>失敗… (何らかのエラー)</translation>
     </message>
 </context>
 </TS>
