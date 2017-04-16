410b908135b1abf8451a3b517be2b094d3a7fede
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2017-04-16 16:42:57 +0900

    Add some necessary fields

--- LoopMID2LoopOGG-CLI/includes/EncoderExecutor.hpp
+++ LoopMID2LoopOGG-CLI/includes/EncoderExecutor.hpp
@@ -2,15 +2,22 @@
 #define ENCODEREXECUTOR_HPP
 
 #include <QtCore>
+#include <QString>
 
 class EncoderExecutor
 {
 protected:
     qreal m_qualityValue;
+    qint32 m_loopStart;
+    qint32 m_loopLength;
+    QString m_binaryPath;
 public:
     EncoderExecutor();
     void setQualityValue(qreal value);
-    virtual int execute();
+    void setLoopStart(qint32 sample);
+    void setLoopLength(qint32 sample);
+    void setBinaryPath(QString binaryPath);
+    virtual int execute(QString inputFilePath, QString outputFilePath){ return -1; }
 };
 
 #endif // ENCODEREXECUTOR_HPP
--- LoopMID2LoopOGG-CLI/src/EncoderExecutor.cpp
+++ LoopMID2LoopOGG-CLI/src/EncoderExecutor.cpp
@@ -1,5 +1,20 @@
 #include "EncoderExecutor.hpp"
 
-EncoderExecutor::EncoderExecutor(): m_qualityValue(0.0f) // should set in derived class constructor
+EncoderExecutor::EncoderExecutor():
+    m_qualityValue(0.0f), // should set in derived class constructor
+    m_loopStart(-1),
+    m_loopLength(-1),
+    m_binaryPath("") // should set in derived class constructor
 {}
-void EncoderExecutor::setQualityValue(qreal value);
+void EncoderExecutor::setQualityValue(qreal value){
+    this->m_qualityValue = value;
+}
+void EncoderExecutor::setLoopStart(qint32 sample){
+    this->m_loopStart = sample;
+}
+void EncoderExecutor::setLoopLength(qint32 sample){
+    this->m_loopLength = sample;
+}
+void EncoderExecutor::setBinaryPath(QString binaryPath){
+    this->m_binaryPath = binaryPath;
+}
