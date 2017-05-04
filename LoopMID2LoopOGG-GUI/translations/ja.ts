55c93916a8e385276adf56a37098f893f29c04c4
Amigo <white.luckers@gmail.com>
2017-05-04 16:07:09 +0900

    Translate: update japanese

--- LoopMID2LoopOGG-GUI/translations/ja.ts
+++ LoopMID2LoopOGG-GUI/translations/ja.ts
@@ -15,79 +15,171 @@
     </message>
     <message>
         <location filename="../forms/ConfigDialog.ui" line="110"/>
+        <source>TiMidity++</source>
+        <translation>TiMidity++</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="147"/>
+        <source>Effects</source>
+        <translation>エフェクト</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="184"/>
+        <source>Encoder</source>
+        <translation>エンコーダ</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="221"/>
         <source>About</source>
         <translation>このソフトについて</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="167"/>
+        <location filename="../forms/ConfigDialog.ui" line="265"/>
         <source>Output File Format</source>
         <translation>出力ファイル形式</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="173"/>
+        <location filename="../forms/ConfigDialog.ui" line="271"/>
         <source>Ogg Vorbis (with loop tag)</source>
         <translation>Ogg Vorbis (ループタグ付き)</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="180"/>
+        <location filename="../forms/ConfigDialog.ui" line="278"/>
         <source>Wave</source>
         <translation>Waveファイル</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="190"/>
+        <location filename="../forms/ConfigDialog.ui" line="288"/>
         <source>Output Mode</source>
         <translation>出力モード</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="196"/>
+        <location filename="../forms/ConfigDialog.ui" line="294"/>
         <source>Optimized mode (for game use)</source>
         <translation>サイズ節約モード (ゲーム素材として利用したい方向け)</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="203"/>
+        <location filename="../forms/ConfigDialog.ui" line="301"/>
         <source>Options for optimized mode</source>
         <translation>サイズ節約モードのオプション</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="209"/>
+        <location filename="../forms/ConfigDialog.ui" line="307"/>
         <source>minimum samples after end of loop</source>
         <translation>ループ終了後に残す最大サンプル数</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="226"/>
+        <location filename="../forms/ConfigDialog.ui" line="324"/>
         <source>Sound-track mode (for listening use)</source>
         <translation>サウンドトラックモード (鑑賞したい方向け)</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="233"/>
+        <location filename="../forms/ConfigDialog.ui" line="331"/>
         <source>Options for sound-track mode</source>
         <translation>サウンドトラックモードのオプション</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="239"/>
+        <location filename="../forms/ConfigDialog.ui" line="337"/>
         <source>number of loop</source>
         <translation>出力に含めるループ回数</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="256"/>
+        <location filename="../forms/ConfigDialog.ui" line="354"/>
         <source>fade-out length (sec.)</source>
         <translation>フェードアウト秒数</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="283"/>
+        <location filename="../forms/ConfigDialog.ui" line="381"/>
         <source>fade-out start time (sec.)</source>
         <translation>ループ後からのフェードアウト開始秒数</translation>
     </message>
     <message>
-        <location filename="../forms/ConfigDialog.ui" line="334"/>
+        <location filename="../forms/ConfigDialog.ui" line="411"/>
+        <source>Paths</source>
+        <translation>各種ファイル/ディレクトリパス</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="417"/>
+        <source>config file</source>
+        <translation>コンフィグファイル</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="429"/>
+        <location filename="../forms/ConfigDialog.ui" line="450"/>
+        <source>Browse...</source>
+        <translation>参照...</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="438"/>
+        <source>soundfont directory</source>
+        <translation>サウンドフォント格納ディレクトリ</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="479"/>
+        <source>Reverb</source>
+        <translation>リバーブ(残響)</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="485"/>
+        <source>Disenable</source>
+        <comment>reverb</comment>
+        <translation>無効</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="492"/>
+        <source>Enable(controlled by MIDI CC)</source>
+        <translation>有効(MIDIコントロールチェンジによる制御)</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="499"/>
+        <source>Enable(override by specified level)</source>
+        <translation>有効(指定レベルで固定)</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="508"/>
+        <source>Reverb Level(1 - 127):</source>
+        <translation>リバーブレベル(1 - 127):</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="547"/>
+        <source>Ogg Vorbis Quality</source>
+        <translation>Ogg Vorbis エンコード品質</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="553"/>
+        <source>Normal(q = 4)</source>
+        <translation>標準(q = 4)</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="560"/>
+        <source>Give priority to size(q = 1)</source>
+        <translation>サイズ優先(q = 1)</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="567"/>
+        <source>Specify custom quality value</source>
+        <translation>品質値を指定</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="576"/>
+        <source>Quality Value(-1 - 10):</source>
+        <translation>品質値(-1 - 10):</translation>
+    </message>
+    <message>
+        <location filename="../forms/ConfigDialog.ui" line="639"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;LoopMID 2 LoopOGG&lt;/span&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:6pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Presented by &lt;/span&gt;&lt;a href=&quot;https://wl-amigo.github.io/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#007af4;&quot;&gt;WhiteLuckers&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;07やさしさゴシック&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;LoopMID 2 LoopOGG&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:6pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:10pt;&quot;&gt;Presented by &lt;/span&gt;&lt;a href=&quot;https://wl-amigo.github.io/&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; text-decoration: underline; color:#007af4;&quot;&gt;WhiteLuckers&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ConfigDialog.cpp" line="49"/>
+        <source>TiMidity++ config file (*.cfg)</source>
+        <translation>TiMidity++ コンフィグファイル (*.cfg)</translation>
     </message>
 </context>
 <context>
@@ -119,6 +211,19 @@ p, li { white-space: pre-wrap; }
         <location filename="../src/ConvertingDialog.cpp" line="37"/>
         <source>Finished!</source>
         <translation>変換完了！</translation>
+    </message>
+</context>
+<context>
+    <name>FileBrowseLineEditConnector</name>
+    <message>
+        <location filename="../src/ConfigDialog.cpp" line="253"/>
+        <source>Select directory...</source>
+        <translation>ディレクトリを選択...</translation>
+    </message>
+    <message>
+        <location filename="../src/ConfigDialog.cpp" line="257"/>
+        <source>Select file...</source>
+        <translation>ファイルを選択...</translation>
     </message>
 </context>
 <context>
@@ -197,23 +302,23 @@ p, li { white-space: pre-wrap; }
         <translation>状態</translation>
     </message>
     <message>
-        <location filename="../src/MultiConvertingDialog.cpp" line="70"/>
+        <location filename="../src/MultiConvertingDialog.cpp" line="75"/>
         <source>pending or converting...</source>
         <translation>変換待機中 or 変換中...</translation>
     </message>
     <message>
-        <location filename="../src/MultiConvertingDialog.cpp" line="103"/>
+        <location filename="../src/MultiConvertingDialog.cpp" line="108"/>
         <source>Finished!</source>
         <translation>変換完了！</translation>
     </message>
     <message>
-        <location filename="../src/MultiConvertingDialog.cpp" line="111"/>
+        <location filename="../src/MultiConvertingDialog.cpp" line="116"/>
         <source>Done!</source>
         <comment>single work</comment>
         <translation>完了！</translation>
     </message>
     <message>
-        <location filename="../src/MultiConvertingDialog.cpp" line="116"/>
+        <location filename="../src/MultiConvertingDialog.cpp" line="121"/>
         <source>Failed by some error...</source>
         <comment>single work</comment>
         <translation>失敗… (何らかのエラー)</translation>
