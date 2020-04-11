9f41dcf44f1d2aa19fb0f5d9c8b441b82b3f3e21
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2020-04-11 12:07:11 +0900

    ref #8 Add slider/spinbox for setting output amplification

--- GUI/includes/mainwindow.h
+++ GUI/includes/mainwindow.h
@@ -11,14 +11,22 @@ class MainWindow;
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
+    Q_PROPERTY(int outputAmp READ outputAmp WRITE setOutputAmp NOTIFY outputAmpChanged)
 
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    int outputAmp() const;
+    void setOutputAmp(int newOutputAmp);
+
 private slots:
     void onBrowseButtonPressed();
     void onPreviewCheckBoxChanged();
+    void onOutputAmpValueChanged(int newOutputAmp);
+
+signals:
+    void outputAmpChanged(int newOutputAmp);
 
 private:
     Ui::MainWindow *ui;
--- GUI/src/mainwindow.cpp
+++ GUI/src/mainwindow.cpp
@@ -18,17 +18,36 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->checkBox, &QCheckBox::toggled, this,
             &MainWindow::onPreviewCheckBoxChanged);
 
+    connect(ui->outputAmpSlider, &QSlider::valueChanged,
+            this, &MainWindow::onOutputAmpValueChanged);
+    connect(ui->outputAmpSpinbox, qOverload<int>(&QSpinBox::valueChanged),
+            this, &MainWindow::onOutputAmpValueChanged);
+    connect(this, &MainWindow::outputAmpChanged,
+            ui->outputAmpSlider, &QSlider::setValue);
+    connect(this, &MainWindow::outputAmpChanged,
+            ui->outputAmpSpinbox, &QSpinBox::setValue);
+
     QSettings settings;
     if (!settings.value("outputDirectory").isNull()) {
         ui->lineEdit->setText(settings.value("outputDirectory").toString());
     }
     ui->checkBox->setChecked(settings.value("preview").toBool());
+    emit outputAmpChanged(this->outputAmp());
 
     connect(ui->actionConfig, &QAction::triggered, this->configDialog,
             &ConfigDialog::show);
 }
 
 MainWindow::~MainWindow() { delete ui; }
+
+int MainWindow::outputAmp() const {
+    return QSettings().value("outputAmp", 100).toInt();
+}
+
+void MainWindow::setOutputAmp(int newOutputAmp) {
+    QSettings().setValue("outputAmp", newOutputAmp);
+    emit outputAmpChanged(newOutputAmp);
+}
 
 void MainWindow::onBrowseButtonPressed() {
     QString outputDirStr = QFileDialog::getExistingDirectory(
@@ -41,4 +60,8 @@ void MainWindow::onBrowseButtonPressed() {
 void MainWindow::onPreviewCheckBoxChanged() {
     QSettings s;
     s.setValue("preview", ui->checkBox->isChecked());
+}
+
+void MainWindow::onOutputAmpValueChanged(int newOutputAmp) {
+    this->setOutputAmp(newOutputAmp);
 }
